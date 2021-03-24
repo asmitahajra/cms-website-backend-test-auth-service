cb0fac9a605e24d18410d05eadd3b94ee49c953b
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-25 01:25:56 +0530

    fix(test-handler): remove verifyToken api call

--- src/handlers/test.handler.js
+++ src/handlers/test.handler.js
@@ -3,9 +3,7 @@ const authService = require('../services/auth.service');
 const testHandler = async (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
   try {
-    const loginUser = await authService.verifyToken(token);
-    console.log(loginUser);
-    res.status(200).send(loginUser);
+    res.status(200).send({message: "test handler run successfully"});
   } catch (error) {
     res.status(500).send();
   }
